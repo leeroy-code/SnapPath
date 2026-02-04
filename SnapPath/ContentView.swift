3697e30ca71024341d85c019422139ae32368ffb
李瑞丰 <liruifeng@qianlima.com>
2026-02-04 09:40:02 +0800

    Initial Commit

---
+++ SnapPath/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnapPath
+//
+//  Created by roy on 2026/2/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnapPath/SnapPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapPathApp.swift
+//  SnapPath
+//
+//  Created by roy on 2026/2/4.
+//
+
+import SwiftUI
+
+@main
+struct SnapPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
